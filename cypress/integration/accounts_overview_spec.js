4f7b2b749f95604d70aa7c7c9c265e0cf93c7c2c
swimm <chanswimmx@hotmail.com>
2020-11-21 14:32:12 -0800

    added getCustomerId function

--- cypress/integration/accounts_overview_spec.js
+++ cypress/integration/accounts_overview_spec.js
@@ -1,5 +1,4 @@
 describe('Accounts Overview', () =>{
-  const customer_id = 12989
   beforeEach(()=>{
     cy.login()
     cy.visit('/overview.htm')
@@ -7,38 +6,42 @@ describe('Accounts Overview', () =>{
   })
 
   it('Balance amount is correct', () =>{
-    cy.request({
-      method:'GET',
-      url:'/services/bank/customers/'+customer_id+'/accounts',
-      headers:{
-        accept:"application/json"
-      }
-    }).then(response=>{
-      var balance = 0
-      for(var i=0; i < response.body.length; i++){
-        balance+=response.body[i].balance;
-      }
-      if(balance >= 0){
-        balance = '$'+ balance
-      }
-      else{
-        balance = '-$'+ Math.abs(balance)
-      }
-      cy.contains(balance)
+    cy.getCustomerId().then((customer_id)=>{
+      cy.request({
+        method:'GET',
+        url:'/services/bank/customers/'+customer_id+'/accounts',
+        headers:{
+          accept:"application/json"
+        }
+      }).then(response=>{
+        var balance = 0
+        for(var i=0; i < response.body.length; i++){
+          balance+=response.body[i].balance;
+        }
+        if(balance >= 0){
+          balance = '$'+ balance
+        }
+        else{
+          balance = '-$'+ Math.abs(balance)
+        }
+        cy.contains(balance)
+      })
     })
   })
 
   it('List of accounts is correct', () =>{
-    cy.request({
-      method:'GET',
-      url:'/services/bank/customers/'+customer_id+'/accounts',
-      headers:{
-        accept:"application/json"
-      }
-    }).then(response=>{
-      for(var i = 0; i<response.body.length;i++){
-        cy.contains(response.body[i].id)
-      }
+    cy.getCustomerId().then((customer_id)=>{
+      cy.request({
+        method:'GET',
+        url:'/services/bank/customers/'+customer_id+'/accounts',
+        headers:{
+          accept:"application/json"
+        }
+      }).then(response=>{
+        for(var i = 0; i<response.body.length;i++){
+          cy.contains(response.body[i].id)
+        }
+      })
     })
   })
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,21 @@
+var username='beans_test'
+var password='123'
+
 Cypress.Commands.add('login', () =>{
   cy.visit('/index.htm')
-  cy.get('input[name=username]').type('beans_test')
-  cy.get('input[name=password]').type('123')
+  cy.get('input[name=username]').type(username)
+  cy.get('input[name=password]').type(password)
   cy.get(':nth-child(5) > .button').click()
+})
+
+Cypress.Commands.add('getCustomerId', ()=>{
+  cy.request({
+    method:'GET',
+    url:'/services/bank/login/'+username+'/'+password,
+    headers:{
+      accept:"application/json"
+    }
+  }).then(response=>{
+    return(response.body.id)
+  })
 })

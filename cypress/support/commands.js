1eb429277d178f8c377df107dd26f4a08ffedfd3
swimm <chanswimmx@hotmail.com>
2020-11-21 14:07:41 -0800

    removed comment

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -4,14 +4,3 @@ Cypress.Commands.add('login', () =>{
   cy.get('input[name=password]').type('123')
   cy.get(':nth-child(5) > .button').click()
 })
-
-// Cypress.Commands.add('login',(user)=>{
-//   cy.request({
-//     url: '/login.htm',
-//     method:'POST',
-//     body:{
-//       username: user.username,
-//       password: user.password,
-//     }
-//   })
-// })
